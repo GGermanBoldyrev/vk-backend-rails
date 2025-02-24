7285cdf1f70ebbc3fb33d7fbbb4aaa4fe680efe5
German Boldyrev <boldyrev_german@mail.ru>
2025-02-24 03:28:34 +0300

    fix: сидер для школ

--- db/migrate/20250223205305_create_schools.rb
+++ db/migrate/20250223205305_create_schools.rb
@@ -2,6 +2,7 @@ class CreateSchools < ActiveRecord::Migration[8.0]
   def change
     create_table :schools do |t|
       t.string :name, null: false
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_23_205307) do
   end
 
   create_table "schools", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
